702bd35d5a11071bd9332d3dc84bdcc764b8be55
anggiaziyah <anggiaziyah61@gmail.com>
2025-07-03 10:37:17 +0700

    edit

--- lib/Screen/auth/signup_screen.dart
+++ lib/Screen/auth/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
 
